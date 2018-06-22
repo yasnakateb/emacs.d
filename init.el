ee5ac7d5a2dcbe09f486107f3e36529376d77c51
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2018-06-22 03:51:12 +0100

    Add window configs

--- init.el
+++ init.el
@@ -18,6 +18,8 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/configs/org_mode.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/configs/buffer.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/configs/window.org"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,4 +31,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
