2fdf336704b88d59e001a1bf36237c7e55e3e1c1
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2018-06-22 07:34:21 +0100

    Add expand-region

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (hungry-delete beacon yasnippet-snippets yasnippet jedi which-key use-package try ox-reveal org-bullets htmlize flycheck dracula-theme counsel auto-complete ace-window))))
+    (expand-region hungry-delete beacon yasnippet-snippets yasnippet jedi which-key use-package try ox-reveal org-bullets htmlize flycheck dracula-theme counsel auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
