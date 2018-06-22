5d0d38ee2e2f68351c00cc232487068010133037
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2018-06-22 04:40:02 +0100

    Add avy

--- init.el
+++ init.el
@@ -20,17 +20,3 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/configs/buffer.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/configs/window.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (counsel whick-key which-key use-package try org-bullets))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
