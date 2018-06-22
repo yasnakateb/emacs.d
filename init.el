1abda8850ab6baf37043cfbd564480e0b99c248f
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2018-06-22 04:16:58 +0100

    Add ivy and swiper

--- init.el
+++ init.el
@@ -25,7 +25,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (whick-key which-key use-package try org-bullets))))
+ '(package-selected-packages
+   (quote
+    (counsel whick-key which-key use-package try org-bullets))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
