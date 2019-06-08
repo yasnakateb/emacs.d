c713802b899d6d4aecce50785d1172ac1553b505
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-06-08 01:28:12 +0100

    New bindings

--- bindings/keys.el
+++ bindings/keys.el
@@ -1,27 +1,50 @@
 ;;; leader file
 (general-define-key
+ :prefix "SPC"
+ :keymaps 'normal
+ "" '(nil :which-key "My lieutenant general prefix")
+ ;; bind nothing but give SPC f a description for which-key
+ "f" '(:ignore t :which-key "Files")
+ "o" '(:ignore t :which-key "Org")
+ "a" '(:ignore t :which-key "Applications")
+ "g" '(:ignore t :which-key "Magit")
+ "b" '(:ignore t :which-key "Buffers")
+ "S" '(:ignore t :which-key "Search")
+ "s" '(:ignore t :which-key "Shell")
+ "h" '(:ignore t :which-key "Help!")
+ "w" '(:ignore t :which-key "Windows")
+ "t" '(:ignore t :which-key "Toggles")
+
+ )
+;;; Files
+(general-define-key
    :prefix "SPC f"
    :states '(normal visual motion)
    :keymaps 'override
    "f" 'counsel-find-file
    "x" 'counsel-M-x
-   "s" 'swiper
-   "t" 'neotree-toggle
-   "I" 'rtags-imenu
-   "i" 'imenu
    )
-;;; leader modes
+;;; Applications
 (general-define-key
-   :prefix "SPC m"
+   :prefix "SPC a"
    :states '(normal visual motion)
    :keymaps 'override
    "m" 'mu4e
-   "g" 'magit-status
-   "o" 'org-mode
    "t" 'twit
-   "r" 'md4rd
-   )
-;;; leader buffer
+   "r" 'md4rd)
+;;; Magit
+(general-define-key
+   :prefix "SPC g"
+   :states '(normal visual motion)
+   :keymaps 'override
+   "s" 'magit-status)
+;;; Org
+(general-define-key
+   :prefix "SPC o"
+   :states '(normal visual motion)
+   :keymaps 'override
+   "o" 'org-mode)
+;;; Buffers
 (general-define-key
    :prefix "SPC b"
    :states '(normal visual motion)
@@ -29,9 +52,8 @@
    "b" 'ibuffer
    "d" 'kill-current-buffer
    "D" 'kill-buffer
-   "r" 'darkroom-mode
    )
-;;; leader window
+;;; Windows
 (general-define-key
    :prefix "SPC w"
    :states '(normal visual motion)
@@ -43,24 +65,25 @@
    "j" 'evil-window-down
    "k" 'evil-window-up
    "l" 'evil-window-right
-   "t" 'toggle-transparency
    )
-;;; leader shell
+;;; Shell
 (general-define-key
    :prefix "SPC s"
    :states '(normal visual motion)
    :keymaps 'override
+   "s" 'eshell-toggle
    "u" 'update-ticket
-   "s" 'scp-project
+   "c" 'scp-project
    "w" 'wttrin
    "W" 'wttrin-exit
    )
-;;; leader goto 
+;;; Search 
 (general-define-key
-   :prefix "SPC g"
+   :prefix "SPC S"
    :states '(normal visual motion)
    :keymaps 'override
    "c" 'avy-goto-char
+   "s" 'swiper
    )
 ;;; leader config files 
 (defun configs-visit ()
@@ -74,10 +97,20 @@
    )
 ;;; information
 (general-define-key
-   :prefix "SPC i"
+   :prefix "SPC h"
    :states '(normal visual motion)
    :keymaps 'override
    "g" 'google-this
    "G" 'google-this-search
    "m" 'man
    )
+;;; Toggles
+(general-define-key
+   :prefix "SPC t"
+   :states '(normal visual motion)
+   :keymaps 'override
+   "n" 'neotree-toggle
+   "I" 'rtags-imenu
+   "r" 'darkroom-mode
+   "t" 'toggle-transparency
+   "i" 'imenu)
