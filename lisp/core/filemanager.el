8ba1ae6ae7f9628302a8bd36c0e4de8ac2c6c175
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-06-09 03:57:38 +0100

    Dired: A separated file

---
+++ lisp/core/filemanager.el
@@ -0,0 +1,39 @@
+;;; reuse buffer
+(require 'dired-single)
+(defun my-dired-init ()
+  "Bunch of stuff to run for dired, either immediately or when it's
+   loaded."
+  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse))
+(if (boundp 'dired-mode-map)
+        (my-dired-init)
+  (add-hook 'dired-load-hook 'my-dired-init))
+;;; using ls-lisp with these settings gives case-insensitve
+(require 'ls-lisp)
+(setq dired-listing-switches "-lhG")
+(setq ls-lisp-use-insert-directory-program nil)
+(setq ls-lisp-ignore-case t)
+(setq ls-lisp-use-string-collate nil)
+;;; customise the appearance of the listing
+(setq ls-lisp-verbosity '(links uid))
+(setq ls-lisp-format-time-list '("%b %e %H:%M" "%b %e  %Y"))
+(setq ls-lisp-use-localized-time-format t)
+;; allow editing file permissions
+(setq wdired-allow-to-change-permissions t)
+;;; preview files in dired
+(use-package peep-dired
+  :ensure t
+  :defer t ; don't access `dired-mode-map' until `peep-dired' is loaded
+  :init
+  (setq peep-dired-cleanup-on-disable t)
+;  (setq peep-dired-cleanup-eagerly t)
+  (setq peep-dired-enable-on-directories t)
+  (setq peep-dired-ignored-extensions '("mkv" "iso" "mp4")))
+;;narrow dired to match filter
+(use-package dired-narrow
+  :ensure t)
+(use-package dired-subtree
+  :ensure t)
+(use-package dired-quick-sort
+  :ensure t
+  :config
+  (dired-quick-sort-setup))
