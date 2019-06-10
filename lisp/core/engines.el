753d37e42315f3487c9c16b3f662e0d6acf062a3
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-06-10 05:10:53 +0100

    Helm: Always at the bottom

--- lisp/core/engines.el
+++ lisp/core/engines.el
@@ -3,4 +3,9 @@
   :init
   (helm-mode 1)
   :config
+  (add-to-list 'display-buffer-alist
+                    `(,(rx bos "*helm" (* not-newline) "*" eos)
+                         (display-buffer-in-side-window)
+                         (inhibit-same-window . t)
+                         (window-height . 0.35)))
   (define-key helm-find-files-map "\t" 'helm-execute-persistent-action))
