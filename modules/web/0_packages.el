74abe546bf5133b3118a99f618aaa0e7a4f31245
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-06-11 13:55:51 +0100

    Web: Added web-mode and js2-mode

---
+++ modules/web/0_packages.el
@@ -0,0 +1,56 @@
+;;; packages.el --- Web -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  M.R. Siavash Katebzadeh
+
+;; Author: M.R.Siavash Katebzadeh <mr.katebzadeh@gmail.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; web-mode
+(use-package web-mode
+  :ensure t)
+
+;;; company-web-html
+(use-package company-web
+  :ensure t)
+
+;;; react native
+(use-package js2-mode
+  :ensure t)
+
+(use-package nodejs-repl
+  :ensure t)
+
+(use-package tide
+  :ensure t)
+
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;;; packages.el ends here
